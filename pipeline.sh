c1e45c4da9db835affa7db692d3008600a0d6ff8
ritik-malik <ritik18406@iiitd.ac.in>
2021-02-09 01:48:55 +0530

    init script for the pipeline

---
+++ pipeline.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# This the starting script for the new pipeline [the 1st script to be run]
+# Initializes and declares all the variables - (dates, time, prefixes, limit, etc.)
+# Then pass the array to master.sh
+# See EOD for structure of the array - "vars"
+# Make sure you are in same dir as scripts & it has ISP_ASN folder
+#
+# Usage -> ./beta_pipeline.sh
+
+echo
+echo "========================================="
+echo "<<<===== WELCOME TO THE PIPELINE =====>>>"
+echo "========================================="
+echo
+
+vars=()
+while [ ${#vars[@]} -lt 30 ]
+do
+    echo -e "`expr 30 - ${#vars[@]}` dates remaining...\n"
+
+    read -p "Enter the year in format YYYY : " YYYY
+    cal -y ${YYYY}
+
+    read -p "Select a month MM (01-12) : " MM
+    cal -d ${YYYY}-${MM}
+
+    read -p "Enter start date DD : " SD
+    read -p "Enter end date DD : " ED
+
+    for i in $(seq -w $SD $ED); do vars+=(${YYYY}${MM}${i}); done
+    echo "Total ${#vars[@]} dates added..."
+
+done
+
+echo
+read -p "Enter the 1st timestamp : " TIME_1
+read -p "Enter the 2nd timestamp : " TIME_2
+read -p "Enter the 3rd timestamp : " TIME_3
+read -p "Enter the 4th timestamp : " TIME_4
+
+vars+=(${TIME_1}); vars+=(${TIME_2})
+vars+=(${TIME_3}); vars+=(${TIME_4})
+
+echo
+read -p "Enter name of the folder containing prefixes in ISP_ASN format : " ISP_ASN 
+vars+=(${ISP_ASN})
+
+read -p "Enter the max() - min() % LIMIT for making graphs XX : " LIMIT
+vars+=(${LIMIT})
+
+echo -e "\n____________________________________________________________\n"
+echo -e "Here's what you entered :\n"
+for i in "${vars[@]}"; do echo "$i"; done
+
+echo -e "\nPrefix files in ${ISP_ASN} :\n"
+for i in $(ls ${ISP_ASN}); do echo $i; done
+
+
+echo -e "\n____________________________________________________________\n"
+echo -e '\nAre you sure you want to proceed?\nOnce started the code will run for 3/2 day,\nonly way to stop it is to kill through\n`ps -ef | grep master.sh`\n& then `kill -9 PID`...\n'
+echo -e "____________________________________________________________\n"
+
+read -p 'Please Type : "YES START THE PIPELINE" : ' ans
+
+###########################################################
+
+if [ "${ans}" = "YES START THE PIPELINE" ]; then
+
+    echo "<<<-------This is the log file maintained by the program-------->>>" >> logs.txt
+    echo -e "\nCurrent value choosen by user" >> logs.txt
+    for i in "${vars[@]}"; do echo "$i"; done >> logs.txt
+    echo "Prefix files in ${ISP_ASN} :" >> logs.txt
+    for i in $(ls ${ISP_ASN}); do echo $i; done >> logs.txt
+    echo "Now calling master.sh for main task..." >> logs.txt
+
+    nohup ./master.sh "${vars[@]}" &
+
+    echo -e "\nYou can now sit back and relax while we give you email notifications about the progress...\n"
+    echo -e 'Else you can watch the [Official logs in `tail -f logs.txt`] or\n[Unofficial logs in `tail -f nohup.out`]'
+    echo -e "You can also close this terminal window safely...\n"
+
+else
+    echo -e "Input error detected! Exiting the program...\nBye\n"
+fi
+
+# Structure of the array VARS :
+#
+# $vars{[0]} = 1st date [start] YYYYMMDD
+# $vars{[1]} = 2nd date
+# $vars{[2]} = 3rd date
+# .
+# .
+# $vars{[29]} = 30th date [end]
+#
+# $vars{[30]} = timestamp_1 TTTT
+# $vars{[31]} = timestamp_2 
+# $vars{[32]} = timestamp_3 
+# $vars{[33]} = timestamp_4 
+#
+# ${vars[34]} = ISP_ASN [folder name]
+#
+# ${vars[35]} = LIMIT XX
+#
+# This array is passed to master.sh
+#
+#
