82a668e3aa1b0631aea416f200b16e267455f4df
ritik-malik <ritik18406@iiitd.ac.in>
2021-02-15 21:26:09 +0530

    download data from routeviews for 1 day

--- routeviews.py
+++ routeviews.py
@@ -2,7 +2,9 @@ import os
 import sys  
 
 # download data from all mirrors of routeviews for a particular date & timestamp
-# output format rib.YYYYMMDD.TTTT_MIRROR
+# output format rib.YYYYMMDD.TTTT.MIRROR
+#
+# usage -> python routeviews.py YYYYMMDD.TTTT
 
 def get_data(x, URL, mirrors):
         
@@ -15,13 +17,14 @@ def get_data(x, URL, mirrors):
 
             path='rib.'+x+'.bz2'                     # path = rib.20200215.1200.bz2
             print("\nRunning bgpscanner....\n")
-            temp = 'bgpscanner -o rib.'+x+'_'+i+' '+path
+            temp = 'bgpscanner -o rib.'+x+'.'+i+' '+path
             os.system(temp)
         
-            os.remove(path)
+            os.remove(path)     # remove .bz2 file
+                                # final output is a ribs_dumps from BGPscanner -> rib.YYYYMMDD.TTTT.MIRROR
 
         except:
-            print("\nFileNotFound....\n")
+            print("\nRIBS_DUMPS_404 -> {}\n".format(x,i))   # for std error msg in nohup.out
 
 URL = 'http://archive.routeviews.org/route-views.'
 mirrors = ['chicago','chile','eqix','flix','gorex','isc','kixp','jinx','linx','napafrica','nwax','phoix',
